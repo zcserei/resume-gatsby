230577e1f7ce306eb01726eb703edde53f7a8c24
zcserei <z@holist.io>
2018-03-16 16:45:31 +0100

    palette update

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,8 @@
 const theme = {
   colors: {
     alto: 'rgba(215, 215, 215, 1)',
-    red_ribbon: 'rgba(224, 13, 72, 1)',
-    mineshaft: 'rgba(22, 22, 22, 1)',
+    red_ribbon: 'rgba(0, 193, 204, 1)',
+    mineshaft: 'rgba(37, 37, 42, 1)',
     white: 'rgba(255, 255, 255, 1)',
     shade: 'rgba(255, 255, 255, 0.05)',
     gray: 'rgba(213, 212, 213, 1)'

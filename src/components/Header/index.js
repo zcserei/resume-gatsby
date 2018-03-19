682f46e8655ec80f87f54f4c4cb057e65b175e99
zcserei <z@holist.io>
2018-03-19 11:01:37 +0100

    header text arrange

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,20 +3,24 @@ import styled from 'react-emotion'
 
 const Name = styled('h1')`
   background-color: ${props => props.theme.colors.red_ribbon};
-  font-size: 4rem;
+  font-size: 3rem;
+  width: 100%;
   display: inline-block;
   padding: 1rem;
   margin-bottom: 0;
   margin-top: 0;
   box-sizing: border-box;
+
+  @media (min-width: 759px) {
+    width: auto;
+  }
 `
 
 const Motto = styled('h2')`
-  color: ${props => props.theme.colors.white};
+  color: ${props => props.theme.colors.red_ribbon};
   margin: 1rem 0 0 1rem;
   font-size: 1.6rem;
   font-weight: 300;
-  line-height: 1.5;
 
   @media (min-width: 769px) {
     font-size: 2rem;
